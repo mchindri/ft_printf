f71ae44d7f30b810d1fad7a334ae100fb8da3ac9
Mihail CHINDRIS <mchindri@e1p78.cluj.42.fr>
2016-01-04 16:02:19 +0100

    da

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,39 @@
 /*   By: mchindri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/30 09:48:34 by mchindri          #+#    #+#             */
-/*   Updated: 2015/12/30 13:52:38 by mchindri         ###   ########.fr       */
+/*   Updated: 2016/01/04 16:00:49 by mchindri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+char	*ft_find_procent(char *p, int *printd_ch)
+{
+	int		ok;
+	char	*q;
+
+	while (*p)
+	{
+		q = CONVERTOR;
+		ok = 0;
+		while (*q && ok == 0)
+			if (*q == *p)
+				ok = 1;
+			else
+				q++;
+		if (ok = 1)
+			return (p);
+		write(1, p, 1);
+		(*printed_ch)++;
+		p++;
+	}
+	return (NULL);
+}
+
+char	*ft_find_form_convertor(char *p)
+{
+
+}
 
 int		ft_printf(const char *format, ...)
 {
@@ -18,12 +46,14 @@ int		ft_printf(const char *format, ...)
 	char	*p;
 	char	*q;
 	char	*conv_spec;
+	int		printed_ch;
 
+	printed_ch = 0;
 	va_start(ap, format);
 	p = (char *)format;
 	while (1)
 	{
-		p = ft_find_procent(p);
+		p = ft_find_procent(p, &printed_ch);
 		if (p == NULL)
 			break;
 		q = ft_find_form_convert(p + 1);
--- ft_printf.h
+++ ft_printf.h
@@ -6,15 +6,25 @@
 /*   By: mchindri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/30 09:50:01 by mchindri          #+#    #+#             */
-/*   Updated: 2015/12/30 10:32:56 by mchindri         ###   ########.fr       */
+/*   Updated: 2016/01/04 15:55:14 by mchindri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdarg.h>
-#include <unistd.h>
+# include <stdarg.h>
+# include <unistd.h>
+
+# define CONVERTOR "sSpdDioOuUxXcC%"
+
+typedef struct	s_type_format
+{
+	char	*flags;
+	int		min_weidth;
+	int		precision;
+	char	length_modefier[2];
+}				t_type_format;
 
 int	ft_printf(const char *format, ...);
 
